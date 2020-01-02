9cb2abc563dc037d567282b8d955aad6b9a5d67c
book <bookgvi@gmail.com>
2020-01-02 12:59:51 +0300

    create main content page

---
+++ src/router/routes.js
@@ -0,0 +1,9 @@
+const routes = [
+  {
+    path: '/',
+    name: 'main',
+    component: () => import('../views/Main')
+  }
+]
+
+export default routes
---
+++ src/views/Main.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="row">
+    <h1>This is main content</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
