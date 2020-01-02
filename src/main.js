3f68e4a06d038024addcb5c1fab17938c7b1e21a
book <bookgvi@gmail.com>
2020-01-02 13:14:34 +0300

    add bootstrap plugin

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './custom.scss'
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
